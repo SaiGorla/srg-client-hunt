171a0ce8ff0c65d2db827250ac132350fa153633
Sai rohith gorla <sairohithgorla01@gmail.com>
2021-01-24 19:42:18 -0600

    initial commit

---
+++ init_locations.js
@@ -0,0 +1,25 @@
+// export default object with the initialization data
+
+export default [
+    {
+   Name: "Yayy ! Colden Pond One",
+   North: 30.351133,
+   West: -44.882717,
+   South: 30.351049,
+   East: -44.882375,
+   Notes: '',
+   Attachments: '',
+   'Quest-Location': 'Colden Pond One',
+ },
+ {
+   Name: 'Bearcat Stadium',
+   North: 20.350438,
+   West: -84.883363,
+   South: 20.350296,
+   East: -84.883180,
+   Notes: '',
+   Attachments: '',
+   'Quest-Location': 'Bearcat Stadium',
+ },
+
+];
