f9e3b06cd6fc0db4c7217756153d32086a8b4130
Sai rohith gorla <sairohithgorla01@gmail.com>
2021-01-27 11:30:53 -0600

    Commit

--- init_locations.js
+++ init_locations.js
@@ -1,25 +1,15 @@
 // export default object with the initialization data
 
 export default [
-    {
-   Name: "B D Owens Library",
-   North: 30.351133,
-   West: -44.882717,
-   South: 30.351049,
-   East: -44.882375,
-   Notes: '',
-   Attachments: '',
-   'Quest-Location': 'B D Owens Library',
- },
- {
-   Name: 'Bearcat Stadium',
-   North: 20.350438,
-   West: -84.883363,
-   South: 20.350296,
-   East: -84.883180,
-   Notes: '',
-   Attachments: '',
-   'Quest-Location': 'Bearcat Stadium',
- },
+  {
+    Name: 'Horizons Apartments',
+    Latitude: 40.3559640,
+    Longitude: -94.8813817
+},
+{
+    Name: 'Forest village Apartments',
+    Latitude: 40.3562558,
+    Longitude: -94.8863981
+}
 
 ];
--- scripts/main.js
+++ scripts/main.js
@@ -1,74 +1,82 @@
-// import default object with a local camelCase name
+  
+
 import locationsArray from '../init-locations.js';
 
-const inside = (device, bounds) => {
-  // console.log(`CHECKING inside ${bounds.Name}`);
-  // console.log(device);
-  // console.log(bounds);
-  // console.log(device.latitude > bounds.South);
-  // console.log(device.latitude < bounds.North);
-  // console.log(device.longitude > bounds.West);
-  // console.log(device.longitude < bounds.East);
-  const ans =
-    device.latitude > bounds.South &&
-    device.latitude < bounds.North &&
-    device.longitude > bounds.West &&
-    device.longitude < bounds.East;
-  // console.log(`CHECKING ${bounds.Name} ANS: ${ans}`);
-  return ans;
-};
 
-/**
- * Get the location
- * Uses new import / export - be sure to set type="module" in HTML
- * Can be easily added to any web page.
- * Includes GeoLocation API example.
- * @module location/getLocation
- * @author Sai Rohith Gorla
- */
-export default function getLocation() {
-  if (!navigator.geolocation) {
-    document.querySelector('#error-message').innerHTML =
-      'Browser does not support geolocation.';
-  } else {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
+let locationElement = document.getElementById("location");
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        document.querySelector('#device-lat').innerHTML = '';
-        document.querySelector('#device-long').innerHTML = '';
-        document.querySelector('#locationAnswer').innerHTML = 'THE HUNT GAME';
+window.addEventListener('load', main);
+locationElement.addEventListener('click', locationHandler);
+locationElement.addEventListener('touch', locationHandler);
 
-        if (position === undefined) {
-          document.querySelector('#error-message').innerHTML =
-            'Browser cannot determine device position (position is undefined).';
+function main() {
+    console.log('Page is fully loaded');
+}
+
+// initializing the current position lat and lon and error to true
+let currentlat;
+let currentlon;
+let error = true;
+
+// collects current location
+async function getLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    }).then(position => {
+        return position;
+    });
+}
+
+// gets current locations and compares the locations from init-locations.js
+// if error is true provides you're not 30 meters nearer to any place
+// if error is false won't show any thing
+async function locationHandler() {
+    let locText = await getLocation();
+    currentlat = locText.coords.latitude;
+    document.getElementById("device-lat").innerHTML = "This is about device-lat: " + currentlat.toFixed(6);
+    currentlon = locText.coords.longitude;
+    document.getElementById("device-long").innerHTML = "This is about device-long: " + currentlon.toFixed(6);
+
+    locationsArray.forEach(function (value) {
+        if (isInside(value.Latitude, value.Longitude)) {
+            document.getElementById("locationAnswer").innerHTML = value.Name;
+            error = false;
         }
-        const device = position.coords;
-        document.querySelector('#device-lat').innerHTML = device.latitude;
-        document.querySelector('#device-long').innerHTML = device.longitude;
-        const arrayLength = locationsArray.length;
-        for (let i = 0; i < arrayLength; i += 1) {
-          const thisLoc = locationsArray[i];
-          if (inside(device, thisLoc)) {
-            const name = thisLoc.Name;
-            document.querySelector('#locationAnswer').innerHTML = name;
-            const utterance = new SpeechSynthesisUtterance();
-            utterance.text = `Congratulations! You found location ${name}`;
-            window.speechSynthesis.speak(utterance);
-            break;
-          }
-        }
-      },
-      (err) => {
-        const s = `ERROR(${err.code}): ${err.message}`;
-        console.warn(s);
-        document.querySelector('#error-message').innerHTML = err.message;
-      },
-      options,
-    );
-  }
+    });
+
+    if(error) {
+        document.getElementById("error-message").innerHTML = "You're not 30 meters nearer to any place.";
+    } else {
+        document.getElementById("error-message").innerHTML = "";
+    }
+}
+
+//Calculates distance and checks distance is below or above 30 meters
+function isInside(questLat, questLon) {
+    let distance = distanceBetweenLocations(questLat, questLon);
+    console.log("distance: " + distance);
+    if (distance < 30) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//Calculate distance between Latitude/Longitude points
+//Returns the distance in meters
+function distanceBetweenLocations(questLat, questLon) {
+    const R = 6371e3;
+    const φ1 = currentlat * Math.PI / 180;
+    const φ2 = questLat * Math.PI / 180;
+    const Δφ = (questLat - currentlat) * Math.PI / 180;
+    const Δλ = (questLon - currentlon) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c;
+    return d; // in metres
 }
