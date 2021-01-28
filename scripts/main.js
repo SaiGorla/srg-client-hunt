e06e8a480064fc29281bcc0a06ed544fcdbc13b5
Sai rohith gorla <sairohithgorla01@gmail.com>
2021-01-28 00:34:34 -0600

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,82 +1,186 @@
   
 
 import locationsArray from '../init-locations.js';
+ 
+
+let currentlat, currentlon;
+
+let error;
 
 
-let locationElement = document.getElementById("location");
 
-window.addEventListener('load', main);
-locationElement.addEventListener('click', locationHandler);
-locationElement.addEventListener('touch', locationHandler);
+ 
 
-function main() {
-    console.log('Page is fully loaded');
-}
-
-// initializing the current position lat and lon and error to true
-let currentlat;
-let currentlon;
-let error = true;
-
-// collects current location
 async function getLocation() {
+
     return new Promise((resolve, reject) => {
+
         navigator.geolocation.getCurrentPosition(resolve, reject);
+
     }).then(position => {
+
         return position;
+
     });
+
 }
 
-// gets current locations and compares the locations from init-locations.js
-// if error is true provides you're not 30 meters nearer to any place
-// if error is false won't show any thing
+ 
+
 async function locationHandler() {
-    let locText = await getLocation();
+
+    const locText = await getLocation();
+
+    // document.getElementById('locationAnswer').innerHTML = locText;
+
+    console.log("------location handler")
+
     currentlat = locText.coords.latitude;
-    document.getElementById("device-lat").innerHTML = "This is about device-lat: " + currentlat.toFixed(6);
+
+    document.getElementById("device-lat").innerHTML =  currentlat.toFixed(6);
+
     currentlon = locText.coords.longitude;
-    document.getElementById("device-long").innerHTML = "This is about device-long: " + currentlon.toFixed(6);
+
+    document.getElementById("device-long").innerHTML = currentlon.toFixed(6);
+
+
+
+
 
     locationsArray.forEach(function (value) {
-        if (isInside(value.Latitude, value.Longitude)) {
-            document.getElementById("locationAnswer").innerHTML = value.Name;
-            error = false;
+
+        console.log(value,"---inside location array")
+
+        if (isInside(value.latitude, value.longitude)) {
+
+            console.log(value.name,"----inside if")
+
+
+
+            
+
+     document.getElementById("locationAnswer").innerHTML = value.Name;       
+
+     error = false;
+
         }
+
     });
 
-    if(error) {
-        document.getElementById("error-message").innerHTML = "You're not 30 meters nearer to any place.";
-    } else {
+
+
+    if(error)
+
+    {
+
+        document.getElementById("error-message").innerHTML = "You are not near to any place.";
+
+    }else {
+
         document.getElementById("error-message").innerHTML = "";
+
     }
+
 }
 
-//Calculates distance and checks distance is below or above 30 meters
-function isInside(questLat, questLon) {
-    let distance = distanceBetweenLocations(questLat, questLon);
-    console.log("distance: " + distance);
-    if (distance < 30) {
-        return true;
-    } else {
-        return false;
-    }
+
+
+function clearErrorText() {
+
+    document.getElementById('error-message').innerHTML = '';
+
 }
+
+
+
+//Calculates distance
+
+function isInside(questLat, questLon) {
+
+    let distance = distanceBetweenLocations(questLat, questLon);
+
+    console.log("distance: " + distance);
+
+    if (distance < 30) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+
+
+
 
 
 //Calculate distance between Latitude/Longitude points
-//Returns the distance in meters
+
 function distanceBetweenLocations(questLat, questLon) {
+
     const R = 6371e3;
+
     const φ1 = currentlat * Math.PI / 180;
+
     const φ2 = questLat * Math.PI / 180;
+
     const Δφ = (questLat - currentlat) * Math.PI / 180;
+
     const Δλ = (questLon - currentlon) * Math.PI / 180;
 
+
+
     const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+
         Math.cos(φ1) * Math.cos(φ2) *
+
         Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
+
+
     const d = R * c;
-    return d; // in metres
+
+    return d; 
+
 }
+
+
+
+// declare main method................
+
+function main() {
+
+    console.log('Starting main method...');
+
+
+
+    // get references to html elements
+
+    const locationElement = document.getElementById('location');
+
+    const errorElement = document.getElementById('error-message');
+
+
+
+    // init error to empty string
+
+    errorElement.innerHTML = '';
+
+
+
+    locationElement.addEventListener('click', locationHandler);
+
+    locationElement.addEventListener('touch', locationHandler);
+
+}
+
+
+
+// this is where it begins
+
+window.addEventListener('load', main);
